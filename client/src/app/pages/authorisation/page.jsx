748a0ef36e0e47480b969b88502da436fe903f47
Unity <Unity@rbiu.local>
2026-09-05 13:01:23 +0500

    update

---
+++ client/src/app/pages/authorisation/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Authorisation() {
+  return (
+    <div>
+        <form>
+          <input type="text" name="login"/>
+          <input type="text" name="password"/>
+        </form>
+    </div>
+  );
+}
